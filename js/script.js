be07556c62bc57bb33cd3fc7db80a704868ec285
prmkvlad <prmk.vlad@gmail.com>
2024-01-30 16:58:57 +0200

    Add hero section

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+// Burger menu
+
 const burger = document.querySelector('.menu__icon');
 const menu = document.querySelector('.menu');
 const body = document.body;
@@ -7,5 +9,24 @@ if (burger && menu) {
 		burger.classList.toggle('_active');
 		menu.classList.toggle('_active');
 		body.classList.toggle('_lock');
+	})
+}
+
+// Filter dropdown
+
+const filter = document.querySelector('.filter');
+
+if (filter) {
+	const items = filter.querySelectorAll('.block-filter');
+
+	items.forEach(item => {
+		item.addEventListener('click', event => {
+			item.querySelector('.block-filter__dropdown').classList.toggle('_active');
+			item.querySelector('.block-filter__icon').classList.toggle('_active');
+
+			if (event.target.classList.contains('block-filter__item')) {
+				item.querySelector('.block-filter__value').textContent = event.target.textContent;
+			}
+		})
 	})
 }
