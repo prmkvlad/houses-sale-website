51a1bf89f7a8ac02284bddf4a075760b210c11e9
prmkvlad <prmk.vlad@gmail.com>
2024-04-02 18:01:54 +0300

    add advantages section

--- js/script.js
+++ js/script.js
@@ -4,8 +4,14 @@ const burger = document.querySelector('.menu__icon');
 const menu = document.querySelector('.menu');
 const body = document.body;
 
+function topFunction() {
+	body.scrollTop = 0; // For Safari
+	document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
 if (burger && menu) {
 	burger.addEventListener('click', () => {
+		topFunction();
 		burger.classList.toggle('_active');
 		menu.classList.toggle('_active');
 		body.classList.toggle('_lock');
